e866ff863ab396c5308a3c574c13121dad746f50
Junwei Low <lowjunwei99@gmail.com>
2020-04-20 14:15:55 +0800

    step 1

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
 from flask import Flask
-from flask_restful import Resource,Api
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-api = Api(app)
+app.config["SECRET_KEY"] = "mysecretkey"
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir,"data.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {"hello":"world"}
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),unique=True)
+    email = db.Column(db.String(50),unique=True)
 
-api.add_resource(HelloWorld,"/")
+    def __repr__(self):
+        return f"Username:{self.username}, Email:{self.email}"
+
+    def __init__(self,username,email):
+        self.username = username
+        self.email = email
+
+db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
